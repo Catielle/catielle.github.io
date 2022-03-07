e28c42764ef1d15bce7863cf57879d5fa0d2d8d3
Victor Toso <me@victortoso.com>
2022-03-07 16:23:54 +0100

    add portuguese
    Signed-off-by: Victor Toso <me@victortoso.com>

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ git init
 echo "Installing Pico theme : "
 echo "git submodule add https://github.com/negrel/hugo-theme-pico.git ./themes/pico"
 git submodule add -f https://github.com/negrel/hugo-theme-pico.git ./themes/pico
+cp pt.toml ./themes/pico/i18n/pt.toml
 
 ./update.sh
 
