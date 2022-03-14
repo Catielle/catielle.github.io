b48ccdab14558f9f01a2a35914302dfa2c4d3949
Victor Toso <me@victortoso.com>
2022-03-14 16:30:22 +0100

    remove submodules
    Signed-off-by: Victor Toso <me@victortoso.com>

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ git init
 
 echo "Installing Pico theme : "
 echo "git submodule add to ./themes/pico"
-git submodule add -f https://github.com/Catielle/hugo-theme-pico.git ./themes/pico
+#git submodule add -f https://github.com/Catielle/hugo-theme-pico.git ./themes/pico
 
 ./update.sh
 
